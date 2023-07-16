bc4e2bd779505cbe82775054c749077668f1e400
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-07-16 10:22:43 -0400

    added comments in js file and added modified README file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # simplePassword_generator
+
+## Password Generator
+My motivation behind creating this app was to make password generation easier for users. It is simple to use and gives you various options like length of password and use of special characters etc. for creating a unique password. 
+
+
+## Installation
+Browser to open the link: 
+
+
+## Credits
+W3schools: https://www.w3schools.com/css/default.asp
+MDN Web Docs: https://developer.mozilla.org/en-US/
+Source code from Rutgers Gitlab: https://git.bootcampcontent.com/Rutgers-University/RUT-VIRT-FSF-PT-06-2023-U-LOLC/-/tree/main/03-JavaScript/02-Challenge
+Rutgers TA 
+
+
+## License
+Licensed under MIT
--- script.js
+++ script.js
@@ -5,33 +5,29 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   password = "";
   conditionArray = [];
+  upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  lower = 'abcdefghijklmnopqrstuvwxyz';
+  special = '!@#$%^&*()?<>{}[]|';
+  number = '1234567890';
   var length = parseInt(prompt("Please enter length between 8-128"));
-  if // checking the length condition
+  if // checking the length and character conditions
     (Number.isNaN(length) || length < 8 || length > 128) {
     alert("password length should be between 8 to 128 charactes. Click on Generate button again and select length again");
   }
   else {
     var uppercase = confirm("Do you want upper case to be include");
-
+    console.log(uppercase);
     var lowercase = confirm("Do you want lower case to be include");
-
+    console.log(lowercase);
     var numberCase = confirm("Do you want to add numbers");
-
+    console.log(numberCase);
     var special_character = confirm("Do you want special character to be include");
-    // checking the character condition
-    if (lowercase === false && uppercase === false && special_character === false && number === false) {
-      alert("select atleast one character type and click generate button again");
-    }
+    console.log(special_character);
   }
-  //creating string of the values that should be used 
-  upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  
-  lower = 'abcdefghijklmnopqrstuvwxyz';
-  
-  special = '!@#$%^&*()?<>{}[]|';
-
-  number = '1234567890';
-  // adding the string value in condition Array if user selects it
+  if (uppercase === false && lowercase === false && special_character === false && numberCase === false) {
+      alert("select atleast one character type and click generate button again");
+  }
+ else { //if conditions are met, adding true conditions to the condition Array
   if (uppercase === true){
     conditionArray.push(upper);
   }
@@ -44,6 +40,7 @@ function generatePassword() {
   if (special_character === true){
     conditionArray.push(special);
   }
+ }
  // inner loop to go through all true values in conditionArray and create random value from it
  // outer loop to create the random values of length more than specified 
   for (var i =0; i<(length); i=i+1){
